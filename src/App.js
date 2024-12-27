3fa0d2bf868b6af33d501ce05f03aa27bc7448c6
Anas Aqeel <aqeelanas352@gmail.com>
2024-12-27 18:02:55 +0500

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React lakdsjfkljfsdkljds f
+          coder ki marni
         </a>
       </header>
     </div>
